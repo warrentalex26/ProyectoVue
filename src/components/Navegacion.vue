dfc385493776a791a9047ae18e310d1f9662c120
warrentalex26 <warrentalex26ies@gmail.com>
2021-02-16 14:29:47 -0600

    propiedades css dinamicas

--- src/components/Navegacion.vue
+++ src/components/Navegacion.vue
@@ -1,9 +1,9 @@
 <template>
   <ul class="nav">
             <li class="nav-item">
-              <a class="nav-link" v-bind:class="{ 'active' : mostrarActivo }" href="#">Active</a>
+              <a class="nav-link active" href="#">Active</a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item" v-bind:style="{ backgroundColor:colorRandom, border: '1px solid yellow' }">
               <a class="nav-link" href="#">Link</a>
             </li>
             <li class="nav-item">
@@ -20,7 +20,8 @@ export default {
     name: "Navegacion",
     data(){
         return{
-            mostrarActivo: true
+            mostrarActivo: true,
+            colorRandom: "#" + Math.floor(Math.random()*6777215).toString(16)//floor redondea hacia abajo un numero, random nos genera un numero aleatorio y lo multiplicamos por el maximo valor posible, y lo convertimos a cadena de texto con 16 para que devuelva un valor hexadecimal
         };
     }
 }
