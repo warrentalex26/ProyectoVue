8ce38ac1f5354f04001370c2d17d368ab3e83db9
warrentalex26 <warrentalex26ies@gmail.com>
2021-02-16 13:21:22 -0600

    iterar objetos con v-for

--- src/components/MiComponente.vue
+++ src/components/MiComponente.vue
@@ -1,37 +1,61 @@
 <template>
+  <div>
+    {{ mensajes.texto2 }}
+    <div v-html="mensajes.texto2"></div>
     <div>
-        {{mensajes.texto2}}
-        <div v-html="mensajes.texto2">
-            
-        </div>
-        <div>
-            <!--<h1>{{colores[1]}}</h1>-->
-            <ul>
-                <li v-for="color in colores" v-bind:key="color">Color: {{color}}</li>
-            </ul>
-        </div>
+      <!--<h1>{{colores[1]}}</h1>-->
+
+      <!--<ul>
+        <li v-for="color in colores" v-bind:key="color">Color: {{ color }}</li>
+      </ul>-->
+
+    <div v-for="color in colores2" v-bind:key="color">
+        {{color.nombre}} -
+        {{color.nombreIngles}}
+        <p>--------------------------</p>
     </div>
+
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'MiComponente',
-    data(){
-        return {
-            libro: {
-                autor: "Alexander Talavera",
-                titulo: "Las mil y una noche"
-            },
-            mensajes:{
-                texto1: "Cadena de texto",
-                texto2: "<h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae, nisi!</h1> <p>Etiqueta P</p>"
-            },
-            colores:["Rojo", "Verde", "Amarillo", "Azul"]
+  name: "MiComponente",
+  data() {
+    return {
+      libro: {
+        autor: "Alexander Talavera",
+        titulo: "Las mil y una noche",
+      },
+      mensajes: {
+        texto1: "Cadena de texto",
+        texto2:
+          "<h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae, nisi!</h1> <p>Etiqueta P</p>",
+      },
+      colores: ["Rojo", "Verde", "Amarillo", "Azul"],
+      colores2: [
+        {
+          nombre: "Rojo",
+          nombreIngles: "red",
+        },
+        {
+          nombre: "Verde",
+          nombreIngles: "green",
+        },
+        {
+          nombre: "Amarillo",
+          nombreIngles: "Yellow",
+        },
+        {
+          nombre: "Morado",
+          nombreIngles: "purple",
         }
-    }
-}
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
